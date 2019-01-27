91e45ecbd16927068a053046a9a2b78bb14eefdb
Joel Vicente <joel_antony_23@hotmail.com>
2019-01-27 00:14:37 -0500

    examen html js

--- Clase05/ExamenHtml5_Js/ExamenHtml5_Js/Content/js/helpers.js
+++ Clase05/ExamenHtml5_Js/ExamenHtml5_Js/Content/js/helpers.js
@@ -12,7 +12,6 @@
             return resultado;
            
         },
-
         getAniosArray: function (anioInicio) {
 
             var hoy = new Date();
@@ -27,19 +26,16 @@
             return anios;
 
         },
-
         BloquearControles: function () {
 
             $('input, select, button, textarea').attr('disabled', 'disabled');
 
         },
-
         DesbloquearControles: function () {
 
             $('input, select, button, textarea').removeAttr('disabled', 'disabled');
 
         },
-
         SetObjetctLocalStorage: function (jsObject, keyStorage) {
             //stringify: SERIALIZA
             var objectString = JSON.stringify(jsObject);
@@ -47,14 +43,33 @@
             localStorage.setItem(keyStorage, objectString);
 
         },
-
         GetObjetctLocalStorage: function (keyStorage) {
             //parse: DESERIALIZA
             var objectString = localStorage.getItem(keyStorage);
             var objectJS = JSON.parse(objectString);
             
             return objectJS;
+        },
+        ValidarRequerido: function (elementID, message = "Este campo es requerido") {
 
+            var element = $("#" + elementID);
+            var value = element.val();            
+
+            if ( value.trim().length === 0 ) {
+                element[0].setCustomValidity(message);
+            }
+
+        },
+        ValidarNumero: function (elementID, message = "Debe ingresar un número") {
+
+            var element = $("#" + elementID);
+            var value = element.val();
+            var isNum = (!isNaN(value) && value.trim().length > 0);
+
+            if (!isNum) {
+                element[0].setCustomValidity(message);
+            }
+            
         }
                
     }
